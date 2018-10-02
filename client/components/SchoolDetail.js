f721e8a8171a032928eba9958ffcdcb183cc720f
Tom Licitra <tom.licitra@gmail.com>
2018-10-01 23:35:43 -0400

    cleaned up backend

--- client/components/SchoolDetail.js
+++ client/components/SchoolDetail.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { deleteSchool } from '../store/schools';
+import StudentTable from './StudentTable';
 
-const SchoolDetail = ({school, deleteSchool, props}) => {
+const SchoolDetail = ({school, schoolStudents, deleteSchool, props}) => {
   return (
     <div className="school">
       <h2>{school.name}</h2>
@@ -17,13 +18,16 @@ const SchoolDetail = ({school, deleteSchool, props}) => {
       <button type="button" onClick={() => deleteSchool(school, props.history)}>Delete School</button>
       <button type="button"><Link to={props.location.pathname + '/edit'}>Edit School</Link></button>
       <button type="button"><Link to="/schools">Return to Schools</Link></button>
+      <hr />
+      <StudentTable students={schoolStudents} />
     </div>
   )
 }
 
 const mapStateToProps = (state, ownProps) => ({
   school: ownProps.school,
-  props: ownProps.props
+  props: ownProps.props,
+  schoolStudents: state.students.filter( student => student.school && student.school.id === ownProps.school.id)
 })
 
 const mapDispatchToProps = (dispatch) => ({
